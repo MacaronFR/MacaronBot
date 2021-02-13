19ea3aeb4646a5c3d2d3d4690c2c154ce8245e5c
MacaronFR <denisft77@gmail.com>
2021-02-13 12:35:19 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const {Client} = require('discord.js');
+const config = require("./config.json");
+
+const client = new Client({
+	partials: ['GUILD_MEMBER','CHANNEL','USER','REACTION']
+});
+
+client.on('ready', ()=>{
+	console.log("Connecté");
+	
+	client.user.setActivity('Manager', {type: 'STREAMING'});
+});
+
+client.on('message',(msg)=>{
+	if(msg.content === 'ping'){
+		msg.reply("pong")
+		.catch(console.error);
+	}
+});
+
+client.login(config.token);
